3564eccee3f55c2acd4a72a24b348dc4ebbd33ae
Aoi12 <hamarovdima88@gmail.com>
2022-01-29 14:48:41 +0600

    ахахахахахаха

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const path = require('path');
+const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
 
 app.get('/',(req,res) =>{
     res.sendFile(__dirname + "/index.html")
 })
 
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+
 app.use(express.static(__dirname + '/assets'))
+
+app.post('/url', function(req, res) {
+    const url = req.body.url;
+
+    res.send(url);
+});
 
 io.on('connection', (socket) => {
     socket.on('соообщение чата', (data) => {
@@ -20,6 +33,3 @@ io.on('connection', (socket) => {
 
 
 
-http.listen(3000, () =>{
-    console.log('запуск сервера')
-})
